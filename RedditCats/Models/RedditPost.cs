6da3c457dad4f056d24148c36f6085bec0d2f19c
neil0609 <neilengracia@yahoo.com>
2016-08-15 01:06:01 -0700

    First Commit

---
+++ RedditCats/Models/RedditPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedditCats.Models
+{
+    public class RedditPost
+    {
+        public string Author { get; set; }
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
